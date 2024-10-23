944b10d959e2171680e607f352ba1fb17f1c1e52
smoscoso <smoscoso@titanium-group.net>
2024-10-23 15:35:33 -0400

    controllers add

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,9 @@
+export const getAllTasks = (req, res) => res.send("Obteniendo tareas");
+
+export const getTask = (req, res) => res.send("Obteniendo tarea unica");
+
+export const createTask = (req, res) => res.send("Creando tarea");
+
+export const updateTask = (req, res) => res.send("Actualizando tarea unica");
+
+export const deleteTask = (req, res) => res.send("Eliminando tarea");
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
+import {
+  createTask,
+  deleteTask,
+  getAllTasks,
+  getTask,
+  updateTask,
+} from "../controllers/tasks.controller.js";
+
 const router = Router();
 
-router.get("/tasks", (req, res) => res.send("Obteniendo tareas"));
-router.get("/tasks/:id", (req, res) => res.send("Obteniendo tarea unica"));
-router.post("/tasks", (req, res) => res.send("Creando tarea"));
-router.put("/tasks/:id", (req, res) => res.send("Actualizando tarea unica"));
-router.delete("/tasks/:id", (req, res) => res.send("Eliminando tarea"));
+router.get("/tasks", getAllTasks);
+router.get("/tasks/:id", getTask);
+router.post("/tasks", createTask);
+router.put("/tasks/:id", updateTask);
+router.delete("/tasks/:id", deleteTask);
 
 export default router;
